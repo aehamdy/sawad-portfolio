3915ebbe2dde98bfb6b49e730f4e4cbf175244be
AhmedEssam <ae.devverse@gmail.com>
2025-06-11 23:20:15 +0300

    Feat: Create summary data container file

---
+++ src/data/summary.js
@@ -0,0 +1,32 @@
+export const achievements = [
+  {
+    id: "experience",
+    number: "12",
+    labels: ["years of", "experience"],
+  },
+  {
+    id: "projects",
+    number: "46",
+    labels: ["projects", "completed"],
+  },
+  {
+    id: "clients",
+    number: "20",
+    labels: ["worldwide", "clients"],
+  },
+];
+
+export const banners = [
+  {
+    id: 1,
+    icon: "layers",
+    skills: ["dynamic animation", "motion design"],
+    to: "",
+  },
+  {
+    id: 2,
+    icon: "layout",
+    skills: ["framer", "figma", "wordpress", "react js"],
+    to: "",
+  },
+];
