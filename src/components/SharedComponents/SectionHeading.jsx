bf7231cd66f05532db087a57b798b900695531cc
AhmedEssam <ae.devverse@gmail.com>
2025-06-11 17:15:09 +0300

    Feat: Create SectionHeading component

---
+++ src/components/SharedComponents/SectionHeading.jsx
@@ -0,0 +1,14 @@
+import Heading from "./Heading";
+
+function SectionHeading({ mainText, highlightText }) {
+  return (
+    <Heading
+      as="h1"
+      className="main-heading flex flex-col items-start w-fit font-bold text-heading-primary uppercase leading-20"
+    >
+      {mainText} <span className="text-heading-secondary">{highlightText}</span>
+    </Heading>
+  );
+}
+
+export default SectionHeading;
