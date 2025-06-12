3d617f83f3c179878bb68949a5305124a7086bb1
AhmedEssam <ae.devverse@gmail.com>
2025-06-12 15:23:55 +0300

    Create projects.js that contains all projects

---
+++ src/data/projects.js
@@ -0,0 +1,51 @@
+const revoProject = "../assets/projects/revo.avif";
+const najmProject = "../assets/projects/najm-ai.avif";
+const nashraProject = "../assets/projects/nashra.avif";
+const ruyaProject = "../assets/projects/Ruya.avif";
+const seeraProject = "../assets/projects/Seera.avif";
+const stabraqProject = "../assets/projects/Stabraq.avif";
+
+export const projects = [
+  {
+    id: 1,
+    name: "Revo",
+    description: "Free Framer Template",
+    image: revoProject,
+    link: "https://google.com",
+  },
+  {
+    id: 2,
+    name: "Najm AI",
+    description: "SaaS Framer Template",
+    image: najmProject,
+    link: "https://google.com",
+  },
+  {
+    id: 3,
+    name: "Nashra",
+    description: "SaaS Framer Template",
+    image: nashraProject,
+    link: "https://google.com",
+  },
+  {
+    id: 4,
+    name: "Ruya",
+    description: "SaaS Framer Template",
+    image: ruyaProject,
+    link: "https://google.com",
+  },
+  {
+    id: 5,
+    name: "Seera",
+    description: "Portfolio Framer Template",
+    image: seeraProject,
+    link: "https://google.com",
+  },
+  {
+    id: 6,
+    name: "Stabraq",
+    description: "Portfolio Framer Template",
+    image: stabraqProject,
+    link: "https://google.com",
+  },
+];
