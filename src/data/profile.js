09298588ebd92a184b2263081dab2db9793133d6
AhmedEssam <ae.devverse@gmail.com>
2025-06-12 18:34:06 +0300

    Create profile data file

---
+++ src/data/profile.js
@@ -0,0 +1,32 @@
+import profileImage from "../assets/profile-picture.webp";
+
+export const profile = {
+  name: "Aabid Ahmad",
+  profileImage: profileImage,
+  socialLinks: [
+    {
+      id: 1,
+      name: "Dribbble",
+      link: "https://dirbbbe.com",
+      icon: "dribbble",
+    },
+    {
+      id: 2,
+      name: "Twitter",
+      link: "https://twitter.com",
+      icon: "twitter",
+    },
+    {
+      id: 3,
+      name: "Instagram",
+      link: "https://instagram.com",
+      icon: "instagram",
+    },
+    {
+      id: 4,
+      name: "Email",
+      link: "https://google.com",
+      icon: "envelope",
+    },
+  ],
+};
