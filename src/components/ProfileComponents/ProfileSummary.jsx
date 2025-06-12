f519334aaa1037dd51f22ab65493fac4efd17504
AhmedEssam <ae.devverse@gmail.com>
2025-06-12 18:31:20 +0300

    Create ProfileSummary component

---
+++ src/components/ProfileComponents/ProfileSummary.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProfileSummary() {
+  return (
+    <p className="w-[77%] mx-auto font-medium text-lg text-content-secondary">
+      A Software Engineer who has developed countless innovative solutions.
+    </p>
+  );
+}
+
+export default ProfileSummary;
