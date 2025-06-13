455501ffe47a7c16d7b3af0026666daa7c593743
AhmedEssam <ae.devverse@gmail.com>
2025-06-13 16:55:19 +0300

    Create ToolItem component

---
+++ src/components/ToolsComponents/ToolItem.jsx
@@ -0,0 +1,23 @@
+import ListItemHeading from "../SharedComponents/ListItemHeading";
+import ListItemParagraph from "../SharedComponents/ListItemParagraph";
+
+function ToolItem({ tool }) {
+  return (
+    <li className="flex gap-4 p-4">
+      <div className="w-[60px] overflow-hidden">
+        <img
+          src={tool.image}
+          alt={`${tool.name} image`}
+          className="object-cover"
+        />
+      </div>
+
+      <div className="text-start">
+        <ListItemHeading text={tool.name} />
+        <ListItemParagraph text={tool.purpose} />
+      </div>
+    </li>
+  );
+}
+
+export default ToolItem;
