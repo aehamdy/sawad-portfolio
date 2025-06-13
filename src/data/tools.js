89ad7015d074f14d6094f3bdebcd4aace62e6d10
AhmedEssam <ae.devverse@gmail.com>
2025-06-13 16:56:43 +0300

    Create used tools data file

---
+++ src/data/tools.js
@@ -0,0 +1,45 @@
+import framerImage from "../assets/tools/framer.webp";
+import figmaImage from "../assets/tools/figma.webp";
+import lemonImage from "../assets/tools/lemon.webp";
+import chatgptImage from "../assets/tools/chat-gpt.webp";
+import notionImage from "../assets/tools/notion.webp";
+import nextjsImage from "../assets/tools//nextjs.webp";
+
+export const tools = [
+  {
+    id: 1,
+    image: framerImage,
+    name: "Framer",
+    purpose: "Website Builder",
+  },
+  {
+    id: 2,
+    image: figmaImage,
+    name: "Figma",
+    purpose: "Design Tools",
+  },
+  {
+    id: 3,
+    image: lemonImage,
+    name: "Lemon Squeezy",
+    purpose: "Payment Provider",
+  },
+  {
+    id: 4,
+    image: chatgptImage,
+    name: "Chat GPT",
+    purpose: "AI Assistant",
+  },
+  {
+    id: 5,
+    image: notionImage,
+    name: "Notion",
+    purpose: "Productivity Tool",
+  },
+  {
+    id: 6,
+    image: nextjsImage,
+    name: "Nextjs",
+    purpose: "React Framework",
+  },
+];
