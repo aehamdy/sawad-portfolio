a4c37b68a9c4365668689349e44b72833f6606d4
AhmedEssam <ae.devverse@gmail.com>
2025-06-14 17:56:34 +0300

    Create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer className="mt-8 py-4 text-center">
+      Created by{" "}
+      <a
+        href="https://ahmedessam-portfolio.vercel.app"
+        className="font-medium text-accent-primary"
+      >
+        Ahmed Essam
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
