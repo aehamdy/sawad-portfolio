79e929a6730b1fa41592dc91d2488eae5fa682fa
AhmedEssam <ae.devverse@gmail.com>
2025-06-14 23:36:38 +0300

    Create nav items data file

---
+++ src/data/navItems.js
@@ -0,0 +1,34 @@
+import { ROUTES } from "../constants/routes";
+
+export const navItems = [
+  {
+    id: 1,
+    label: "home",
+    icon: "home",
+    link: ROUTES.HOME,
+  },
+  {
+    id: 2,
+    label: "projects",
+    icon: "folder",
+    link: ROUTES.PROJECTS,
+  },
+  {
+    id: 3,
+    label: "experience",
+    icon: "briefcase",
+    link: ROUTES.EXPERIENCES,
+  },
+  {
+    id: 4,
+    label: "tools",
+    icon: "tool",
+    link: ROUTES.TOOLS,
+  },
+  {
+    id: 5,
+    label: "thoughts",
+    icon: "edit",
+    link: ROUTES.THOUGHTS,
+  },
+];
