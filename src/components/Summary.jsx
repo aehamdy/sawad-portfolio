ae5968f30bf5e7b56fd2f3cdbac3ae5db1d788a2
AhmedEssam <ae.devverse@gmail.com>
2025-06-30 15:05:17 +0300

    Feat: Add animation

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,16 +1,28 @@
 import SummaryIntro from "./SummaryComponents/SummaryIntro";
 import SummaryAchievements from "./SummaryComponents/SummaryAchievements";
 import SummaryBanners from "./SummaryComponents/SummaryBanners";
+import { motion } from "motion/react";
+import {
+  MAIN_VARIANTS,
+  INITIAL_VALUE,
+  END_VALUE,
+} from "../constants/mainAnimation";
 
 function Summary() {
   return (
-    <section className="flex flex-col justify-start gap-[50px]">
+    <motion.section
+      className="flex flex-col justify-start gap-[50px]"
+      variants={MAIN_VARIANTS}
+      initial={INITIAL_VALUE}
+      whileInView={END_VALUE}
+      viewport={{ once: true, amount: 0.25 }}
+    >
       <SummaryIntro />
 
       <SummaryAchievements />
 
       <SummaryBanners />
-    </section>
+    </motion.section>
   );
 }
 
