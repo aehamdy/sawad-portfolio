2b2f18c30b9c80cb3d895179dde2df512a43b815
AhmedEssam <ae.devverse@gmail.com>
2025-07-03 16:22:34 +0300

    Style: Add animations

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,22 @@
+import {
+  MAIN_VARIANTS,
+  INITIAL_VALUE,
+  END_VALUE,
+} from "../constants/mainAnimation";
 import { projects } from "../data/projects";
 import ProjectItem from "./ProjectComponents/ProjectItem";
 import SectionHeading from "./SharedComponents/SectionHeading";
+import { motion } from "motion/react";
 
 function Projects() {
   return (
-    <section className="flex flex-col items-center md:items-start">
+    <motion.section
+      className="flex flex-col items-center md:items-start"
+      variants={MAIN_VARIANTS}
+      initial={INITIAL_VALUE}
+      whileInView={END_VALUE}
+      viewport={{ once: true, amount: 0.25 }}
+    >
       <SectionHeading mainText="recent" highlightText="projects" />
 
       <ul className="w-full flex flex-col gap-5 md:gap-4">
@@ -12,7 +24,7 @@ function Projects() {
           <ProjectItem key={project.id} project={project} />
         ))}
       </ul>
-    </section>
+    </motion.section>
   );
 }
 
