0cd5d8b80ced06414ea9706b73894c5a14ea1bc4
AhmedEssam <ae.devverse@gmail.com>
2025-07-11 18:51:23 +0300

    Style: Add animation

--- src/components/PremiumTools.jsx
+++ src/components/PremiumTools.jsx
@@ -1,10 +1,22 @@
 import { tools } from "../data/tools";
 import SectionHeading from "./SharedComponents/SectionHeading";
 import ToolItem from "./ToolsComponents/ToolItem";
+import { motion } from "motion/react";
+import {
+  END_VALUE,
+  INITIAL_VALUE,
+  MAIN_VARIANTS,
+} from "../constants/mainAnimation";
 
 function PremiumTools() {
   return (
-    <section className="flex flex-col items-center lg:items-start gap-4">
+    <motion.section
+      className="flex flex-col items-center lg:items-start gap-4"
+      variants={MAIN_VARIANTS}
+      initial={INITIAL_VALUE}
+      whileInView={END_VALUE}
+      viewport={{ once: true, amount: 0.25 }}
+    >
       <SectionHeading mainText="premium" highlightText="tools" />
 
       <ul className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
@@ -12,7 +24,7 @@ function PremiumTools() {
           <ToolItem key={tool.id} tool={tool} />
         ))}
       </ul>
-    </section>
+    </motion.section>
   );
 }
 
